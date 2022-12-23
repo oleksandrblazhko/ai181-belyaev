790679db4cfc150290d7979da3d9de243d9acb89
Kirill Belyaev <55207058+VIVAskAVIV@users.noreply.github.com>
2022-12-23 13:51:40 +0200

    Create change_data_secure.sql

---
+++ Laboratory-work-8/change_data_secure.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION change_data(human_name VARCHAR, id INTEGER)
+RETURNS VARCHAR
+AS $$
+DECLARE
+	str VARCHAR;
+BEGIN
+	str := 'UPDATE public.human SET name = $1 WHERE p_id = $2';
+	RAISE NOTICE 'Query=%', str;
+    EXECUTE str USING human_name, id;
+	RETURN human_name;
+END;
+$$ LANGUAGE plpgsql;
