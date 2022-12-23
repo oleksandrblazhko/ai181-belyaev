1e1d41b38c40eec1fe32145b7dbf4a717823a893
Kirill Belyaev <55207058+VIVAskAVIV@users.noreply.github.com>
2022-12-23 14:03:04 +0200

    Update change_data.sql

--- Laboratory-work-8/change_data.sql
+++ Laboratory-work-8/change_data.sql
@@ -1,13 +1,13 @@
-CREATE OR REPLACE FUNCTION change_data(human_name VARCHAR, id INTEGER)
-RETURNS VARCHAR
+CREATE OR REPLACE FUNCTION change_data(name VARCHAR, bd DATE)
+RETURNS DATE
 AS $$
 DECLARE
 	str VARCHAR;
 BEGIN
-	str := 'UPDATE public.human SET name = ''' || human_name || 
-    ''' where p_id = ''' || id|| '''';
+	str := 'UPDATE public.human AS h SET bd = ''' || bd || 
+    ''' where name = ''' || name || '''';
 	RAISE NOTICE 'Query=%', str;
     EXECUTE str;
-	RETURN human_name;
+	RETURN bd;
 END;
 $$ LANGUAGE plpgsql;
